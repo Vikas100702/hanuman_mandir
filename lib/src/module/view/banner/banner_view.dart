2a8afaf5eea0ec2a2d252a01b0a60190a2cbaf26
Ram-Nadendla-Vaap <111744230+apankit12@users.noreply.github.com>
2025-12-18 15:20:49 +0530

    Implements Slider.

--- lib/src/module/view/banner/banner_view.dart
+++ lib/src/module/view/banner/banner_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:hanuman_mandir/src/data/services/banner/banner_service.dart';
 import 'package:hanuman_mandir/src/module/controller/banner/banner_controller.dart';
 import 'package:hanuman_mandir/src/module/model/banner/banner_model.dart';
@@ -13,31 +14,83 @@ class BannerView extends StatelessWidget {
       BannerController(bannerService: BannerService()),
     );
     return Obx(() {
-      if(bannerController.isLoading.value) {
+      if (bannerController.isLoading.value) {
         return const SizedBox(
           height: 250,
           child: Center(child: CircularProgressIndicator()),
         );
       }
 
-      if(bannerController.bannerDataModel.value == null || bannerController.bannerDataList.isEmpty) {
+      if (bannerController.bannerDataModel.value == null ||
+          bannerController.bannerDataList.isEmpty) {
         return const SizedBox.shrink();
       }
 
-      final Datum data = bannerController.bannerDataList.first;
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          // Define a breakpoint for mobile/tablet vs desktop
+          bool isMobile = constraints.maxWidth < 900;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: isMobile ? 200 : 400,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              viewportFraction: 1.0,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+            ),
+            items: bannerController.bannerDataList.map((Datum data) {
+              String fileName = data.refDataName?.split('/').last ?? '';
+              String assetPath = "assets/images/$fileName";
 
-      return LayoutBuilder(builder: (context, constraints) {
-        // Define a breakpoint for mobile/tablet vs desktop
-        bool isMobile = constraints.maxWidth < 900;
-        return Container(
-          height: 50,
-          width: .infinity,
-          color: Colors.red,
-          child: Center(
-            child: Text(data.bannerHeading ?? "Banner Heading"),
-          ),
-        );
-      });
+              return Builder(
+                builder: (BuildContext context) {
+                  return data.bannerType == "ONLY IMAGE"
+                      ? Image.asset(
+                          assetPath,
+                          fit:
+                              .cover /*, errorBuilder: (context, error, stackTrace) {
+                    return const Center(
+                      child: Icon(Icons.error, color: Colors.red),
+                    );
+                  },*/,
+                        )
+                      : !isMobile
+                      ? Row(
+                          children: [
+                            Image.asset(
+                              assetPath,
+                              fit:
+                                  .cover /*, errorBuilder: (context, error, stackTrace) {
+                                            return const Center(
+                                                      child: Icon(Icons.error, color: Colors.red),
+                                            );
+                                          },*/,
+                            ),
+                            Expanded(child: Container(color: Colors.red)),
+                          ],
+                        )
+                      : Column(
+                          children: [
+                            Image.asset(
+                              assetPath,
+                              fit:
+                                  .cover /*, errorBuilder: (context, error, stackTrace) {
+                                            return const Center(
+                                                      child: Icon(Icons.error, color: Colors.red),
+                                            );
+                                          },*/,
+                            ),
+                            Expanded(child: Container(color: Colors.red)),
+                          ],
+                        );
+                },
+              );
+            }).toList(),
+          );
+        },
+      );
     });
   }
 }
