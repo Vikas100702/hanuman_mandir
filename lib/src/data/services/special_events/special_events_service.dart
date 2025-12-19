55c3ddf0058657124d630116bff84b460aba624e
Ram-Nadendla-Vaap <111744230+apankit12@users.noreply.github.com>
2025-12-19 17:08:33 +0530

    Initial Commit

---
+++ lib/src/data/services/special_events/special_events_service.dart
@@ -0,0 +1,8 @@
+/*
+import 'package:hanuman_mandir/src/module/model/special_events/special_events_model.dart';
+
+class SpecialEventsService {
+  Future<SpecialEventsModel> fetchSpecialEventsServiceData() async {
+    return null;
+  }
+}*/
