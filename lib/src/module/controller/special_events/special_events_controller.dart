04a884cbda8255380dc6a67a729492d13b49657c
Ram-Nadendla-Vaap <111744230+apankit12@users.noreply.github.com>
2025-12-19 18:05:55 +0530

    Initial Commit.

---
+++ lib/src/module/controller/special_events/special_events_controller.dart
@@ -0,0 +1,39 @@
+import 'package:get/get.dart';
+import 'package:hanuman_mandir/src/data/services/special_events/special_events_service.dart';
+import 'package:hanuman_mandir/src/module/model/special_events/special_events_model.dart';
+
+class SpecialEventsController extends GetxController {
+  final SpecialEventsService specialEventsService;
+
+  var isLoading = false.obs;
+  var specialEventsDataModel = Rxn<SpecialEventsModel>();
+
+  SpecialEventsController({required this.specialEventsService});
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchSpecialEventsData();
+  }
+
+  Future<void> fetchSpecialEventsData() async {
+    try {
+      isLoading(true);
+      final result = await specialEventsService.fetchSpecialEventsServiceData();
+
+      if (result != null) {
+        specialEventsDataModel.value = result;
+      } else {
+        // Todo : Optional: Handle null case (e.g., show a snackbar)
+        // Get.snackbar("Error", "Failed to fetch Special events data");
+      }
+    } catch (e) {
+      throw Exception("Exception in Special Events Controller : $e");
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  List<Datum> get specialEventsDataList =>
+      specialEventsDataModel.value?.data ?? [];
+}
