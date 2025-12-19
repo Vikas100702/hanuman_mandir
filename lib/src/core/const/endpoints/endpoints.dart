a076960821e2393f535df2ed7503c32cc7119728
Ram-Nadendla-Vaap <111744230+apankit12@users.noreply.github.com>
2025-12-19 16:02:44 +0530

    adding global url

--- lib/src/core/const/endpoints/endpoints.dart
+++ lib/src/core/const/endpoints/endpoints.dart
@@ -1,4 +1,5 @@
 class Endpoints {
+  static const String globalUrl = "https://aspgen.vaaptech.com:9000";
   static const String baseUrl = "https://aspgen.vaaptech.com:9000/api/appgen/filterAPI";
 
 
