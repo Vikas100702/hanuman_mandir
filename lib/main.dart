22a4430d2350c20570e6ebceb79dbd22372b114e
Ram-Nadendla-Vaap <111744230+apankit12@users.noreply.github.com>
2025-12-19 16:07:40 +0530

    Adding HttpOverrides to fix cors policy issue. (Not Solved)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hanuman_mandir/src/module/view/header/header_view.dart';
 import 'package:hanuman_mandir/src/module/view/home_view.dart';
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
