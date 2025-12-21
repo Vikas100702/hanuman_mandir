5c6cb4fd93f9ecaa708dbefb2f77d7c675cfacc8
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2025-12-21 16:03:05 +0530

    Initial Commit

---
+++ lib/src/module/view/todays_panchangam/widgets/todays_panchangam_widgets.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hanuman_mandir/src/core/const/app_colors.dart';
+
+class TodaysPanchangamWidgets {
+  static Widget panchangamCards({
+    required String title,
+    required Widget child,
+    double height = 400,
+    EdgeInsetsGeometry? contentPadding,
+  }) {
+    return Container(
+      height: height,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.shadow,
+            blurRadius: 8,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: .stretch,
+        children: [
+          // Header Section
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            color: AppColors.darkRed,
+            alignment: Alignment.center,
+            child: Text(
+              title.toUpperCase(),
+              style: GoogleFonts.openSans(
+                color: AppColors.white,
+                fontSize: 18,
+                fontWeight: .bold,
+                letterSpacing: 1.1,
+              ),
+            ),
+          ),
+
+          // Body Section
+          Expanded(child: Container(
+            padding: contentPadding ?? const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: AppColors.white,
+            ),
+            child: child,
+          ))
+        ],
+      ),
+    );
+  }
+}
