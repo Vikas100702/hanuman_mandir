76281e37f9d8f06ee6234411766bd8be2f1b15ea
Vikas Verma <vermasingh2002@gmail.com>
2025-12-23 18:35:24 +0530

    Adding customDivider from global widgets

--- lib/src/module/view/todays_panchangam/widgets/todays_panchangam_widgets.dart
+++ lib/src/module/view/todays_panchangam/widgets/todays_panchangam_widgets.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hanuman_mandir/src/core/const/app_colors.dart';
+import 'package:hanuman_mandir/src/module/view/global_widgets/global_widgets.dart';
 
 class TodaysPanchangamWidgets {
   static Widget panchangamCards({
@@ -29,25 +30,35 @@ class TodaysPanchangamWidgets {
             padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
             color: AppColors.darkRed,
             alignment: Alignment.center,
-            child: Text(
-              title.toUpperCase(),
-              style: GoogleFonts.openSans(
-                color: AppColors.white,
-                fontSize: 18,
-                fontWeight: .bold,
-                letterSpacing: 1.1,
-              ),
+            child: Column(
+              children: [
+                Text(
+                  title.toUpperCase(),
+                  style: GoogleFonts.openSans(
+                    color: AppColors.white,
+                    fontSize: 18,
+                    fontWeight: .bold,
+                    letterSpacing: 1.1,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                GlobalWidgets.customDivider(
+                  height: 2,
+                  width: 150,
+                  color: const Color(0xFFD9950B),
+                ),
+              ],
             ),
           ),
 
           // Body Section
-          Expanded(child: Container(
-            padding: contentPadding ?? const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              color: AppColors.white,
+          Expanded(
+            child: Container(
+              padding: contentPadding ?? const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/bg_card.png"))),
+              child: child,
             ),
-            child: child,
-          ))
+          ),
         ],
       ),
     );
