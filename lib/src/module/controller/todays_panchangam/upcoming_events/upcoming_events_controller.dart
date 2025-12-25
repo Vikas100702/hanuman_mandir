a5efeffe59d5c136a688dd2f555377bf8cf8252a
Vikas Verma <vermasingh2002@gmail.com>
2025-12-25 11:00:45 +0530

    Fetching Data on the basis of event end date

--- lib/src/module/controller/todays_panchangam/upcoming_events/upcoming_events_controller.dart
+++ lib/src/module/controller/todays_panchangam/upcoming_events/upcoming_events_controller.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:hanuman_mandir/src/data/services/todays_panchangam/upcoming_events/upcoming_events_service.dart';
 import 'package:hanuman_mandir/src/module/model/todays_panchangam/upcoming_events/upcoming_events_model.dart';
+import 'package:intl/intl.dart';
 
 class UpcomingEventsController extends GetxController {
   final UpcomingEventsService upcomingEventsService;
@@ -35,11 +37,47 @@ class UpcomingEventsController extends GetxController {
     }
   }
 
-  /*List<Datum> get upcomingEventsDataList {
+  List<Datum> get upcomingEventsDataList {
     final events = upcomingEventsDataModel.value?.data ?? [];
 
-    if(events.isEmpty) return [];
+    if (events.isEmpty) return [];
 
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
 
-  }*/
+    final dateFormat = DateFormat("MM/dd/yyyy");
+
+    return events.where((event) {
+      // if (event.startDate == null || event.startDate!.isEmpty) return false;
+      if (event.endDate == null || event.endDate!.isEmpty) return false;
+
+      try {
+        // Parsing 'MM/dd/yyyy' string to DateTime
+        // DateTime parsedDate = dateFormat.parse(event.startDate!);
+        DateTime parsedDate = dateFormat.parse(event.endDate!);
+
+        // Normalizing parsed date to 00:00:00(mid night)
+        final eventStartDate = DateTime(
+          parsedDate.year,
+          parsedDate.month,
+          parsedDate.day,
+        );
+        final eventEndDate = DateTime(
+          parsedDate.year,
+          parsedDate.month,
+          parsedDate.day,
+        );
+
+        // Keeping event if StartDate is Today or in the Future
+        // !isBefore(today) is equivalent to >= today
+        return !eventEndDate.isBefore(today);
+      } catch (e) {
+        // If the date string is malformed or doesn't match MM-dd-yyyy, hide the event
+        debugPrint(
+          'Error parsing date for event ${event.id}: ${event.endDate}',
+        );
+        return false;
+      }
+    }).toList();
+  }
 }
